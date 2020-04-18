54ffada8180c226d87d91eec6835e437ec652372
Warren Harper II <warrenharper2@att.net>
2020-04-18 18:59:57 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,9 +1,9 @@
 class SecretsController < ApplicationController
     before_action :require_login
-  
+
     def show
     end
-  
+
     private
     def require_login
       unless session.include? :name
